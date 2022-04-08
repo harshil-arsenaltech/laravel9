f8348497c797f2f773ba6173f8176753ebb10db5
Harshil Solanki <harshil.solanki@arsenaltech.com>
2022-04-08 12:46:53 +0530

    Login module, IsAdmin middleware  Implemented

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'role' => \jeremykenedy\LaravelRoles\App\Http\Middleware\VerifyRole::class,
         'permission' => \jeremykenedy\LaravelRoles\App\Http\Middleware\VerifyPermission::class,
         'level' => \jeremykenedy\LaravelRoles\App\Http\Middleware\VerifyLevel::class,
+        'isAdmin' => \App\Http\Middleware\IsAdmin::class,
     ];
 }
